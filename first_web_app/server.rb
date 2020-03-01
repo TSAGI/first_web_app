6f83ea5d184888583364ac159182aacce8396a1f
Tal Sagi <Tal@Eusers-MacBook-Pro.local>
2020-03-01 12:18:25 -0500

    first time using sinatra to create web page

--- first_web_app/server.rb
+++ first_web_app/server.rb
@@ -1,13 +1,25 @@
 require 'sinatra'
 
 get '/' do
+    @return = 'This is my first web app.  To learn more about me, click
+    <a href="/about">here.</a> To view my contact info, click
+    <a href="/contact">here.</a>'
+    
     erb :welcome
 end
 
 get '/about' do
+    @return = 'Click
+    <a href="/">here</a> to return to the welcome page, or click
+    <a href="/contact">here</a> to go to the contact page.'
+
     erb :about
 end
 
 get '/contact' do 
+    @return = 'Click
+    <a href="/">here</a> to return to the welcome page, or click
+    <a href="/about">here</a> to go to the about page.'
+
     erb :contact
 end
